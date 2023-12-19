2f0ddc2f393ceb201d2b525d579a025e2f4394ea
abdel-waki <mkddabdel@gmail.com>
2023-12-19 13:49:25 -0800

    high level

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -25,4 +25,4 @@ class MagicClass:
 
     def circumference(self):
         """Return The circumference of the MagicClass."""
-        return (2 * math.pi * self.__radius
+        return (2 * math.pi * self.__radius)
